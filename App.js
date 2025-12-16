e4355a1cd59a2400b4a2089acdce2a6d0cb55cdb
Sethuramalingam Duraisamy <59419619+Sethuramalingam-art@users.noreply.github.com>
2025-12-16 12:13:33 +0400

    Create App.js

---
+++ App.js
@@ -0,0 +1,8 @@
+import './App.css';
+import React, { useState } from 'react'
+
+import MultistepComp from './MultistepComp';
+
+function App() {
+  return <MultistepComp list="5"></MultistepComp>
+}
